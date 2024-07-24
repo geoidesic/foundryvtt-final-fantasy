f7e6749ab84f5e99d0741dfa79c4ae6a9e7f0b3b
Noel da Costa <noeldacosta@gmail.com>
2024-07-24 16:34:58 +0100

    Initial commit

---
+++ src/helpers/constants.ts
@@ -0,0 +1,4 @@
+export const SYSTEM_ID = '';
+export const SYSTEM_NAME = '';
+export const SYSTEM_DESCRIPTION = '';
+export const LOG_PREFIX = `${SYSTEM_ID} | `;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { LOG_PREFIX, SYSTEM_ID } from "~/src/helpers/constants"
+
+
+
+
+
+Hooks.once("init", async () => {
+  log.d('Starting System');
+
+});
+
+Hooks.once("ready", async () => {
+});
