466a29bde11b934f071863a9d8ffab1ee6829297
Noel da Costa <noeldacosta@gmail.com>
2024-08-01 21:51:19 +0100

    fix: could only open one item sheet at a time

--- src/components/applications/ItemSheet.js
+++ src/components/applications/ItemSheet.js
@@ -1,17 +1,18 @@
 import SvelteDocumentSheet from "~/src/documents/DocumentSheet";
 import ItemSheetShell from "./ItemSheetShell.svelte";
 import { SYSTEM_CODE, SYSTEM_ID } from "~/src/helpers/constants";
-import { SvelteApplication } from "@typhonjs-fvtt/runtime/svelte/application";
+import { generateRandomElementId } from "~/src/helpers/utility";
+// import { SvelteApplication } from "@typhonjs-fvtt/runtime/svelte/application";
 
 export default class FF15ItemSheet extends SvelteDocumentSheet {
 
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
-      id: `${SYSTEM_ID}--item-sheet`,
+      id: `${SYSTEM_ID}--item-shee-${generateRandomElementId()}`,
       classes: [SYSTEM_CODE],
       title: game.i18n.localize(`${SYSTEM_CODE}.Types.Item`),
       width: 550,
-      height: 400,
+      height: 500,
       minWidth: 400,
       svelte: {
         class: ItemSheetShell,
