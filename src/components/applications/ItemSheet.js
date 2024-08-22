47432edf6b7354b1be4109c30fa0274a59429879
Noel da Costa <noeldacosta@gmail.com>
2024-08-22 22:37:31 +0100

    #1 - various ports; packs still working

--- src/components/applications/ItemSheet.js
+++ src/components/applications/ItemSheet.js
@@ -1,11 +1,17 @@
 import SvelteDocumentSheet from "~/src/documents/DocumentSheet";
 import ItemSheetShell from "./ItemSheetShell.svelte";
+import { SYSTEM_CODE, SYSTEM_ID } from "~/src/helpers/constants";
+import { generateRandomElementId } from "~/src/helpers/utility";
+// import { SvelteApplication } from "@typhonjs-fvtt/runtime/svelte/application";
 
 export default class FF15ItemSheet extends SvelteDocumentSheet {
 
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
-      width: 500,
+      id: `${SYSTEM_ID}--item-shee-${generateRandomElementId()}`,
+      classes: [SYSTEM_CODE],
+      title: game.i18n.localize(`${SYSTEM_CODE}.Types.Item`),
+      width: 550,
       height: 500,
       minWidth: 400,
       svelte: {
