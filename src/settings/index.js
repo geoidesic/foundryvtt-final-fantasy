6d4ec7a79d4703de0a58572370b12fbe35a226d7
Noel da Costa <noeldacosta@gmail.com>
2024-08-22 22:40:27 +0100

    #1 - various ports; packs still working - 431e241

--- src/settings/index.js
+++ src/settings/index.js
@@ -5,7 +5,7 @@ export function registerSettings() {
 
   /** User settings */
   dontShowWelcome();
-
+  applicationWindowHeaderIconsOnly()
 }
 
 
@@ -13,6 +13,16 @@ function dontShowWelcome() {
   game.settings.register(SYSTEM_ID, 'dontShowWelcome', {
     name: localize(`${SYSTEM_CODE}.Setting.DontShowWelcome.Name`),
     hint: localize(`${SYSTEM_CODE}.Setting.DontShowWelcome.Hint`),
+    scope: 'user',
+    config: true,
+    default: false,
+    type: Boolean,
+  });
+}
+function applicationWindowHeaderIconsOnly() {
+  game.settings.register(SYSTEM_ID, 'applicationWindowHeaderIconsOnly', {
+    name: localize(`${SYSTEM_CODE}.Setting.applicationWindowHeaderIconsOnly.Name`),
+    hint: localize(`${SYSTEM_CODE}.Setting.applicationWindowHeaderIconsOnly.Hint`),
     scope: 'user',
     config: true,
     default: false,
