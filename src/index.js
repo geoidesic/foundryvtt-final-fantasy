dadf3110b25ea92a85987603a5cb5388c136df6c
Noel da Costa <noeldacosta@gmail.com>
2024-08-26 09:19:00 +0100

    Added drum sound to combat start

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import FF15Actor from '~/src/extensions/actor.js'
 import FF15ActorSheet from "~/src/components/applications/ActorSheet";
 import FF15ItemSheet from "~/src/components/applications/ItemSheet";
 import systemconfig from "~/src/helpers/systemconfig.ts"
+import FFChat from "~/src/components/organisms/chat/FFChat.svelte";
 
 //- helpers
 function setupDSN() {
@@ -25,7 +26,6 @@ function setupDSN() {
 }
 
 
-
 //- Foundry Class Extensions
 CONFIG.Actor.documentClass = FF15Actor
 
@@ -33,12 +33,11 @@ CONFIG.Actor.documentClass = FF15Actor
 //- Foundry System Hooks
 Hooks.once("init", async (a, b, c) => {
 
-
   game.system.log = log;
   log.level = log.DEBUG;
   game.system.log.i(`Starting System ${SYSTEM_ID}`);
 
-  // CONFIG.debug.hooks = true;
+  CONFIG.debug.hooks = true;
 
   registerSettings();
   setupModels();
@@ -55,13 +54,38 @@ Hooks.once("init", async (a, b, c) => {
     makeDefault: true,
   });
 
-  
   Hooks.call("gff15.initIsComplete");
 });
 
 Hooks.once("ready", async () => {
-
   if (!game.settings.get(SYSTEM_ID, 'dontShowWelcome')) {
     new WelcomeApplication().render(true, { focus: true });
+  }
+});
+
+Hooks.on("combatStart", async () => {
+  AudioHelper.play({ src: "sounds/drums.wav", volume: 0.8, autoplay: true, loop: false });
+});
+
+/**
+ * Used by chat message demo to manually attach a Svelte component, SurgeRoll, to a chat message.
+ *
+ * Note: You must manually destroy this Svelte component in an associated `preDeleteChatMessage` like the one provided
+ * below. The reason being is that you are manually / conditionally creating a Svelte component that is not monitored /
+ * controlled by TRL itself, so you must also manually destroy this component when the chat message is deleted.
+ */
+Hooks.on('renderChatMessage', (message, html) => {
+  const FFMessage = message.getFlag(SYSTEM_ID, 'data');
+  game.system.log.d('FFMessage', FFMessage);
+  if (typeof FFMessage === 'object') {
+    message._svelteComponent = new FFChat(
+      {
+        target: html[0],
+        props: {
+          FFMessage,
+          messageId: message._id
+        }
+      }
+    )
   }
 });
