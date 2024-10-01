369333bb1e7e492b4c2ff3fd1462c9274f402502
Noel da Costa <noeldacosta@gmail.com>
2024-10-01 20:08:41 +0100

    Added Details tab to Trait item sheet

---
+++ src/components/organisms/item/type/trait/tabs/Details.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { onMount } from 'svelte';
+  import ProseMirror from "~/src/components/molecules/ProseMirror.svelte";
+  </script>
+  <template lang="pug">
+    .item-tabs
+      .flexcol.flex3.left.prose.inset
+        ProseMirror( attr="system.description" )
+  </template>
+  <style lang='sass'>
+    @import '../../../../../../styles/Mixins.sass'
+    .item-tabs
+      @include inset
+  </style>
