ed2820474d9cf8c702237db5ece218da7968fea2
Noel da Costa <noeldacosta@gmail.com>
2024-10-05 18:18:13 +0100

    Added checkbox control

---
+++ src/components/atoms/controls/DocCheckbox.svelte
@@ -0,0 +1,52 @@
+<svelte:options accessors={true} />
+
+<script>
+  import { getContext } from "svelte";
+  import { resolveDotpath } from "~/src/helpers/paths";
+
+  // The value of the input
+  export let valuePath = "";
+  export let document = false;
+  export let preventDefault = false;
+  export let disabled = false;
+  export let label = "";
+  export let editable = false;
+
+  let inputValue,
+    LABEL = !!label,
+    inputElement,
+    pulseClass = ""
+  ;
+
+  // Document reference
+  const doc = document || getContext("#doc");
+
+  const update = async () => {
+    if (preventDefault) return;
+    await $doc.update({[valuePath]: Boolean(inputValue)}); 
+  }
+
+
+</script>
+
+<template lang="pug">
+  div(class="{$$props.class}")
+    +if("$$props.label")
+      label.flex1 {$$props.label}
+    input(
+      {...$$restProps} 
+      type="checkbox" 
+      bind:this="{inputElement}" 
+      bind:checked="{inputValue}" 
+      disabled="{disabled || !$doc.isOwner}"
+      on:change="{update}"
+    )
+
+</template>
+
+<style lang="sass" >
+  .small 
+    width: 14px
+    height: 14px
+  
+</style>
