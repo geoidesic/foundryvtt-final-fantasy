531df8096f191c57e78b931d48180e3e4ac433d9
Noel da Costa <noeldacosta@gmail.com>
2024-10-05 18:50:23 +0100

    Kept points as doubleclick to edit

--- src/components/molecules/Points/BP.svelte
+++ src/components/molecules/Points/BP.svelte
@@ -25,11 +25,11 @@
             .header Max
           .flex.max.header 
             .header 
-              DocInput(type="number" name="BP" min=0 valuePath="system.points.BP.max")
+              DocInput(clickType="dblclick" type="number" name="BP" min=0 valuePath="system.points.BP.max")
 
       .flex.header.val
         .header
-          DocInput(type="number" name="BP" min=0 max="{$actor.system.points.BP.max}" valuePath="system.points.BP.val")
+          DocInput(clickType="dblclick" type="number" name="BP" min=0 max="{$actor.system.points.BP.max}" valuePath="system.points.BP.val")
       
   </template>
 
