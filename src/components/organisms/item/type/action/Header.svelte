4b702d1e83381e65832213d8065337f7109173ad
Noel da Costa <noeldacosta@gmail.com>
2024-10-07 18:25:19 +0100

    Added header descriptions to item sheet for Action

--- src/components/organisms/item/type/action/Header.svelte
+++ src/components/organisms/item/type/action/Header.svelte
@@ -1,10 +1,38 @@
 <script>
-import { onMount } from 'svelte';
+  import { onMount, getContext } from "svelte";
+  const item = getContext("#doc");
+
 </script>
 <template lang='pug'>
   .item-header
+    .flexcol.description-tab
+      .flexrow
+        .flexcol.flex1.left
+          ol.properties-list
+            +if("$item.system.hasCheck")
+              li Check: {$item.system.checkAttribute.toUpperCase()}
+            +if("$item.system.hasCR")
+              li CR: {$item.system.CR.toUpperCase()}
+            +if("$item.system.directHitType")
+              li Direct Hit {$item.system.directHitType.capitalize()}
+                +if("$item.system.directHitDamage")
+                  | : {$item.system.directHitDamage}
+            +if("$item.system.hasHeavierShot")
+              li Heavier Shot Type: {$item.system.heavierShotType}
+            +if("$item.system.hasLimitation")
+              li Limitation: {$item.system.limitation}
+            +if("$item.system.hasRanged")
+              li Range: {$item.system.rangeType}
+            +if("$item.system.hasTarget")
+              li Target: {$item.system.target.capitalize()}
+            +if("$item.system.hasTrigger")
+              li Trigger: {$item.system.trigger}
+
     
 </template>
 <style lang='sass'>
   @import '../../../../../styles/Mixins.sass'
+
+  .description-tab
+    +itemSheetDescriptionTab
 </style>
