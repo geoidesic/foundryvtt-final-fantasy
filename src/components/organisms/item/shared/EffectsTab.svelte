c47e956d44033c2586fa4f7f529f453d80bbacce
Noel da Costa <noeldacosta@gmail.com>
2024-12-02 14:35:19 +0200

    SL-99 fixed for refactor

--- src/components/organisms/item/shared/EffectsTab.svelte
+++ src/components/organisms/item/shared/EffectsTab.svelte
@@ -4,7 +4,7 @@
   import { getContext, setContext, onMount, onDestroy } from "svelte";
   import { rippleFocus } from "@typhonjs-fvtt/runtime/svelte/action/animate";
   import { TJSInput } from "@typhonjs-fvtt/svelte-standard/component";
-  import { createFilterQuery } from "~/src/filters/effectsFilterQuery";
+  import { createFilterQuery } from "~/src/filters/itemFilterQuery";
   import { TJSDocument } from "@typhonjs-fvtt/runtime/svelte/store/fvtt/document";
   import { getEffectOrigin } from "~/src/helpers/util";
   import ScrollingContainer from "~/src/helpers/svelte-components/ScrollingContainer.svelte";
