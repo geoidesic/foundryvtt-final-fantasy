1e1a9636f7c51b889526ea2ab05ea0f12e191405
Noel da Costa <noeldacosta@gmail.com>
2024-12-08 18:20:33 +0200

    #14 - added Favourites first draft

--- src/components/tabs/actor/Attributes.svelte
+++ src/components/tabs/actor/Attributes.svelte
@@ -2,12 +2,17 @@
 import { onMount } from 'svelte';
 import AttributeSection from "~/src/components/organisms/actor/AttributeSection.svelte";
 import PointsSection from "~/src/components/organisms/actor/PointsSection.svelte";
+import Favourites from "~/src/components/organisms/actor/Favourites.svelte";
 
 </script>
 <template lang='pug'>
   .panel.overflow
-    AttributeSection
-    PointsSection
+    .flexrow
+      .flex3
+        AttributeSection
+        PointsSection
+      .flex1
+        Favourites
 </template>
 <style lang='sass'>
   @import '../../../styles/Mixins.sass'
