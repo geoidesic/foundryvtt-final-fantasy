c54ba07a40831c8b48e7d77b2a8a43ac7428b95d
Noel da Costa <noeldacosta@gmail.com>
2024-12-13 07:58:16 +0200

    #27 - moved some properties to base as they will be used by NPCs too

--- src/models/actors/baseActor.js
+++ src/models/actors/baseActor.js
@@ -10,6 +10,73 @@ export class FFActorDataModel extends FFTypeDataModel {
       inventoryLocked: new BooleanField({ initial: false }),
       description: new HTMLField(),
       DOT: new NumberField({ required: true, integer: true, min: 0, initial: 0 }),
+      globalCheckMod: new NumberField({ required: true, integer: true, min: 0, initial: 0 }),
+      points: new SchemaField({
+        MP: new SchemaField({
+          val: new NumberField({ required: true, integer: true, min: 0, initial: 0 }),
+          max: new NumberField({ required: true, integer: true, min: 0, initial: 0 })
+        }),
+        HP: new SchemaField({
+          val: new NumberField({ required: true, integer: true, min: 0, initial: 0 }),
+          max: new NumberField({ required: true, integer: true, min: 0, initial: 0 })
+        }),
+        BP: new SchemaField({
+          val: new NumberField({ required: true, integer: true, min: 0, initial: 0 }),
+          max: new NumberField({ required: true, integer: true, min: 0, initial: 0 })
+        })
+      }),
+      attributes: new SchemaField({
+        primary: new SchemaField({
+          str: new SchemaField({
+            //- @deprecated: I think `label` is in the wrong place? Should maybe just be in the svelte template as it is a constant, not data
+            // label: new StringField({ initial: localize('FF15.Types.Actor.Types.PC.Attributes.Primary.str.Label') }),
+            val: new NumberField({ required: true, integer: true, min: 0, initial: 0 }),
+            mod: new NumberField({ required: true, integer: true, min: 0, initial: 0 })
+          }),
+          dex: new SchemaField({
+            // label: new StringField({ initial: localize('FF15.Types.Actor.Types.PC.Attributes.Primary.dex.Label') }),
+            val: new NumberField({ required: true, integer: true, min: 0, initial: 0 }),
+            mod: new NumberField({ required: true, integer: true, min: 0, initial: 0 })
+          }),
+          vit: new SchemaField({
+            // label: new StringField({ initial: localize('FF15.Types.Actor.Types.PC.Attributes.Primary.vit.Label') }),
+            val: new NumberField({ required: true, integer: true, min: 0, initial: 0 }),
+            mod: new NumberField({ required: true, integer: true, min: 0, initial: 0 })
+          }),
+          int: new SchemaField({
+            // label: new StringField({ initial: localize('FF15.Types.Actor.Types.PC.Attributes.Primary.ing.Label') }),
+            val: new NumberField({ required: true, integer: true, min: 0, initial: 0 }),
+            mod: new NumberField({ required: true, integer: true, min: 0, initial: 0 })
+          }),
+          mnd: new SchemaField({
+            // label: new StringField({ initial: localize('FF15.Types.Actor.Types.PC.Attributes.Primary.mnd.Label') }),
+            val: new NumberField({ required: true, integer: true, min: 0, initial: 0 }),
+            mod: new NumberField({ required: true, integer: true, min: 0, initial: 0 })
+          }),
+        }),
+        secondary: new SchemaField({
+          def: new SchemaField({
+            // label: new StringField({ initial: localize('FF15.Types.Actor.Types.PC.Attributes.Primary.def.Label') }),
+            val: new NumberField({ required: true, integer: true, min: 0, initial: 0 }),
+            mod: new NumberField({ required: true, integer: true, min: 0, initial: 0 })
+          }),
+          mag: new SchemaField({
+            // label: new StringField({ initial: localize('FF15.Types.Actor.Types.PC.Attributes.Primary.mag.Label') }),
+            val: new NumberField({ required: true, integer: true, min: 0, initial: 0 }),
+            mod: new NumberField({ required: true, integer: true, min: 0, initial: 0 })
+          }),
+          vig: new SchemaField({
+            // label: new StringField({ initial: localize('FF15.Types.Actor.Types.PC.Attributes.Primary.vig.Label') }),
+            val: new NumberField({ required: true, integer: true, min: 0, initial: 0 }),
+            mod: new NumberField({ required: true, integer: true, min: 0, initial: 0 })
+          }),
+          spd: new SchemaField({
+            // label: new StringField({ initial: localize('FF15.Types.Actor.Types.PC.Attributes.Primary.spd.Label') }),
+            val: new NumberField({ required: true, integer: true, min: 0, initial: 0 }),
+            mod: new NumberField({ required: true, integer: true, min: 0, initial: 0 })
+          }),
+        }),
+      })
     }
   }
 }
