8b8646d8a417d2098902cfb7e47684c5f861c019
Noel da Costa <noeldacosta@gmail.com>
2024-12-13 08:00:23 +0200

    fix: Rest button alignment wasn't perfect

--- src/components/molecules/TitleBlock.svelte
+++ src/components/molecules/TitleBlock.svelte
@@ -44,9 +44,9 @@
     .flex4.nowrap
       .flexrow.nowrap
         .flex1.rest-button
-          .left
+          .left.mt-xxs
             button.stealth(on:click|preventDefault="{rest}" data-tooltip="{localize('FF15.TitleBlock.Rest')}" aria-label="{localize('FF15.TitleBlock.Rest')}")
-            img.rest-icon(src="systems/foundryvtt-final-fantasy/assets/icons/tabs/tent.webp")
+              img.rest-icon(src="systems/foundryvtt-final-fantasy/assets/icons/tabs/tent.webp")
 
         .flex4.nowrap.font-inter.lvcontainer
           .scaleup LV 
