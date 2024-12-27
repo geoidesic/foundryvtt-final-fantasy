b9e255e204c5dd8289dfd1bd2431dd3f5c4b6b55
Noel da Costa <noeldacosta@gmail.com>
2024-12-27 05:25:54 +0200

    #68 - added getter

--- src/extensions/active-effect.js
+++ src/extensions/active-effect.js
@@ -7,4 +7,15 @@ export default class FFActiveEffect extends ActiveEffect {
       return super.isTemporary;
     }
 
+    /**
+     * Check if the effect is transferred from an item 
+     * (i.e. a passive effect from an item owned by that actor, 
+     * rather than an active effect transferred as a result of an action 
+     * by another actor)
+     * @returns {boolean}
+     */
+    get isTransferred() {
+      return this.flags?.[SYSTEM_ID]?.originInstance.transferredEffects.length > 0;
+    }
+
 }
