4e74c98d0b0f59257ddb56559a04e23d4bcdef8e
Noel da Costa <noeldacosta@gmail.com>
2024-12-30 17:04:42 +0200

    #69 - progress, but it's not saving changes made to field values

--- src/components/tabs/activeEffect/Changes.svelte
+++ src/components/tabs/activeEffect/Changes.svelte
@@ -13,6 +13,36 @@ function deleteChange(index) {
   changes.splice(index, 1);
   $doc.update({ changes });
 }
+function addChange() {
+  const changes = [...$doc.changes || []];
+  changes.push({
+    key: "",
+    mode: null,
+    value: ""
+  });
+  $doc.update({ changes });
+}
+
+function save() {
+  //- validate changes
+  const changes = [...$doc.changes || []];
+  if(changes.length === 0) {
+    $doc.update({ changes: [] });
+    game.system.log.info('No changes to save');
+    return;
+  }
+  for(const change of changes) {
+    if(!change.key || !change.mode || !change.value) {
+      game.system.log.warn('Please complete all fields');
+      ui.notifications.warn('Please complete all fields');
+      return;
+    }
+  }
+  //- save changes
+  game.system.log.g('Saving...', changes);
+  $doc.update({ changes });
+  game.system.log.g('Changes saved');
+}
 
 onMount(async () => {
   console.log('Changes', $doc);
@@ -28,7 +58,7 @@ onMount(async () => {
           th.left.fixed(scope="col") {localize('EFFECT.Change.Mode')}
           th.left.fixed(scope="col") {localize('EFFECT.Change.Value')}
           th.buttons(scope="col")
-            button.stealth(on:click=addChange)
+            button.stealth(on:click!="{() => addChange()}")
               i.fa.fa-plus
       tbody
         +each("$doc?.changes || [] as change, i")
@@ -36,12 +66,15 @@ onMount(async () => {
             td
               DocInput(valuePath="{`changes[${i}].key`}" alwaysEditable="{true}" )
             td
-              //- DocSelect(valuePath="{`changes[${i}].mode`}" options="{activeEffectModes}" )
+              DocSelect(valuePath="{`changes[${i}].mode`}" options="{activeEffectModes}" )
             td
               DocInput(valuePath="{`changes[${i}].value`}" alwaysEditable="{true}" )
             td.buttons
-              //- button.stealth(on:click!="{deleteChange(i)}")
+              button.stealth(on:click!="{() => deleteChange(i)}")
                 i.fa.fa-trash
+
+    button.mt-sm.glossy-button.gold-light.hover-shine(on:click="{save}") - Save Changes
+
 </template>
 <style lang="sass">
   table
