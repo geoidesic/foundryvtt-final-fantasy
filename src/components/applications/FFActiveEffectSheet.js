d2313fdf36747a50d27569d86693e5032b38ff15
Noel da Costa <noeldacosta@gmail.com>
2024-12-30 13:14:16 +0200

    #69 - added dynamic title to the custom ActiveEffect sheet application header

--- src/components/applications/FFActiveEffectSheet.js
+++ src/components/applications/FFActiveEffectSheet.js
@@ -6,6 +6,7 @@ import FFActiveEffectShell from './FFActiveEffectShell.svelte';
 
 export default class FFActiveEffectSheet extends SvelteApplication {
    #doc;
+   #storeUnsubscribe;
 
    constructor(doc, options) {
       super(options);
@@ -22,7 +23,6 @@ export default class FFActiveEffectSheet extends SvelteApplication {
       return foundry.utils.mergeObject(super.defaultOptions, {
          id: `${SYSTEM_ID}--active-effect-sheet-${generateRandomElementId()}`,
          classes: [SYSTEM_CODE, 'sheet', 'active-effect-config'],
-         title: 'EFFECT.ConfigTitle',
          width: 560,
          height: 'auto',
          svelte: {
@@ -44,5 +44,26 @@ export default class FFActiveEffectSheet extends SvelteApplication {
    async _injectHTML(html) {
       this.options.svelte.props.doc = this.document;
       return super._injectHTML(html);
+   }
+
+   render(force = false, options = {}) {
+      if (!this.#storeUnsubscribe) {
+         this.#storeUnsubscribe = this.#doc.subscribe(this.#handleDocUpdate.bind(this));
+      }
+      return super.render(force, options);
+   }
+
+   async close(options = {}) {
+      if (this.#storeUnsubscribe) {
+         this.#storeUnsubscribe();
+         this.#storeUnsubscribe = void 0;
+      }
+      return super.close(options);
+   }
+
+   #handleDocUpdate(doc) {
+      if (doc) {
+         this.reactive.title = `${game.i18n.localize("FF15.EFFECT.ConfigTitle")}: ${doc.name || ''}`;
+      }
    }
 }
