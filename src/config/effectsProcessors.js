84a2861cfba74cc2847510e58cd55ce7ddd6085f
Noel da Costa <noeldacosta@gmail.com>
2025-01-02 16:21:20 +0200

    #37 - added missing hooks

--- src/config/effectsProcessors.js
+++ src/config/effectsProcessors.js
@@ -5,4 +5,14 @@ export function setupEffectsProcessors() {
     const processor = new effects.PrimaryBaseDamageBuff(event.actor);
     processor.process(event);
   });
+
+  Hooks.on('FF15.RerollDamageDice', (event) => {
+    const processor = new effects.RerollDamageDice(event.actor);
+    processor.process(event);
+  });
+
+  Hooks.on('FF15.EnableCombatTurnSlot', async (event) => {
+    const processor = new effects.EnableCombatTurnSlot(event.actor);
+    await processor.process(event);
+  });
 }
