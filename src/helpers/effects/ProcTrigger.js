63603dea33cbc9e70a90ec17e72ff4d31043e55a
Noel da Costa <noeldacosta@gmail.com>
2025-01-07 08:08:39 +0200

    removed debug

--- src/helpers/effects/ProcTrigger.js
+++ src/helpers/effects/ProcTrigger.js
@@ -1,21 +1,14 @@
 import { SYSTEM_ID } from "~/src/helpers/constants";
 export default class ProcTrigger {
   constructor(actor) {
-    game.system.log.d("[PROC] Creating ProcTrigger processor for actor:", actor.name);
     this.actor = actor;
   }
 
   async process(event) {
-    game.system.log.d("[PROC] ProcTrigger processor called with event:", {
-      itemName: event.item.name,
-      hasRoll: !!event.roll,
-      d20Result: event.roll ? event.roll.terms[0].results[0].result : null,
-      procsList: event.item.system.procs?.list
-    });
+    
 
     const { item, roll } = event;
     if (!item.system.procs?.list?.length) {
-      game.system.log.d("[PROC] No procs list found on item");
       return;
     }
 
@@ -31,24 +24,16 @@ export default class ProcTrigger {
       d20Result = procRoll.terms[0].results[0].result;
     }
 
-    game.system.log.d("[PROC] D20 result check:", {
-      d20Result,
-      procTrigger: item.system.procTrigger,
-      meetsThreshold: d20Result >= item.system.procTrigger
-    });
 
     if (!item.system.procTrigger || d20Result < item.system.procTrigger) {
-      game.system.log.d("[PROC] Proc trigger check failed");
       return;
     }
 
     // Process each proc effect
     for (const procRef of item.system.procs.list) {
-      game.system.log.d("[PROC] Processing proc ref:", procRef);
       
       const procItem = await fromUuid(procRef.uuid);
       if (!procItem) {
-        game.system.log.w("[PROC] Could not find proc item from uuid:", procRef.uuid);
         continue;
       }
 
@@ -58,20 +43,13 @@ export default class ProcTrigger {
         i.type === procItem.type
       );
 
-      game.system.log.d("[PROC] Found actor item:", {
-        found: !!actorItem,
-        name: actorItem?.name,
-        hasEffects: actorItem?.hasEffects
-      });
 
       if (!actorItem?.hasEffects) {
-        game.system.log.d("[PROC] Actor item has no effects, skipping");
         continue;
       }
 
       // Enable the effects
       const enabled = await this.actor.enableLinkedEffects(actorItem);
-      game.system.log.d("[PROC] Enabled effects:", enabled);
 
       // Send a chat message for the proc trigger
       await ChatMessage.create({
