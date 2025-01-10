df57a95c9f569b24eee4c30d253b5d23bb0480ea
Noel da Costa <noeldacosta@gmail.com>
2025-01-10 13:44:57 +0200

    #78 - dealt with es linting

--- src/helpers/effects/index.js
+++ src/helpers/effects/index.js
@@ -18,22 +18,4 @@ export default {
   DamageOverTime,
   ProcTrigger
 };
-
-/**
- * Register effect processors with the system
- */
-export function registerEffectProcessors() {
-  // ... other registrations ...
-
-  Hooks.on('FF15.procTrigger', (event) => {
-    const processor = new ProcTrigger(event.actor);
-    processor.process(event);
-  });
-}
-
-/**
- * Process all effects for an actor
- * @param {Actor} actor - The actor to process effects for
- * @param {object} event - The event data
- * @return {Promise<void>} A promise that resolves when all effects are processed
- */
+=
