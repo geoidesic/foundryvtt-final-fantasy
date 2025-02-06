c1293de7e9f52bf7b19aa8a50b833024d36b9950
Noel da Costa <noeldacosta@gmail.com>
2025-02-06 09:43:51 +0200

    Fix: defaultchat refactor was incomplete

--- src/helpers/rolls/handlers/DefaultChatHandler.js
+++ src/helpers/rolls/handlers/DefaultChatHandler.js
@@ -1,8 +1,8 @@
 import { SYSTEM_ID } from "~/src/helpers/constants.js";
 
 /**
- * Handles all action-related operations
-*/
+ * Handles the default chat message for an item
+ */
 export default class DefaultChatHandler {
   /**
    * @param {Actor} actor - The actor this handler is for
@@ -12,38 +12,34 @@ export default class DefaultChatHandler {
   }
 
   /**
-   * Handles the default chat message for an item
    * @param {Item} item - The item to create a chat message for
-   * @return {Promise<ChatMessage>} The created chat message
    */
-  async handle(item) {
-    setTimeout(async () => {
-      return await ChatMessage.create({
-        user: game.user.id,
-        speaker: game.settings.get(SYSTEM_ID, 'chatMessageSenderIsActorOwner')
-          ? ChatMessage.getSpeaker({ actor })
-          : null,
-        flags: {
-          [SYSTEM_ID]: {
-            data: {
-              chatTemplate: 'RollChat',
-              actor: {
-                _id: actor._id,
-                name: actor.name,
-                img: actor.img
-              },
-              item: {
-                _id: item._id,
-                uuid: item.uuid,
-                name: item.name,
-                img: item.img,
-                type: item.type,
-                system: item.system
-              }
+  handle(item) {
+    ChatMessage.create({
+      user: game.user.id,
+      speaker: game.settings.get(SYSTEM_ID, 'chatMessageSenderIsActorOwner')
+        ? ChatMessage.getSpeaker({ actor: this.actor })
+        : null,
+      flags: {
+        [SYSTEM_ID]: {
+          data: {
+            chatTemplate: 'RollChat',
+            actor: {
+              _id: this.actor._id,
+              name: this.actor.name,
+              img: this.actor.img
+            },
+            item: {
+              _id: item._id,
+              uuid: item.uuid,
+              name: item.name,
+              img: item.img,
+              type: item.type,
+              system: item.system
             }
           }
         }
-      });
-    }, 2000);
+      }
+    });
   }
 }
