ad18632bd7de16dc6aaaf5ee8f1866bda471a7c5
Noel da Costa <noeldacosta@gmail.com>
2025-02-11 20:48:33 +0200

    #85 - removed unused method

--- src/hooks/createActiveEffect.js
+++ src/hooks/createActiveEffect.js
@@ -10,30 +10,7 @@ import FFActiveEffect from "~/src/extensions/active-effect.js";
  */
 export default function createActiveEffect() {
   Hooks.on("createActiveEffect", async (activeEffect, data) => {
-    game.system.log.o('[CREATE ACTIVE EFFECT] Starting effect creation:', {
-      name: activeEffect.name,
-      duration: activeEffect.duration,
-      origin: activeEffect.origin
-    });
 
-    // Skip if already has a properly configured duration
-    if (activeEffect.duration?.rounds || activeEffect.duration?.turns) {
-      game.system.log.o('[CREATE ACTIVE EFFECT] Effect already has duration configured:', {
-        name: activeEffect.name,
-        duration: activeEffect.duration
-      });
-      return;
-    }
-    
-    // Use the consolidated duration handling
-    await FFActiveEffect.setCombatDuration(activeEffect);
-
-    game.system.log.o('[CREATE ACTIVE EFFECT] Final effect state:', {
-      name: activeEffect.name,
-      duration: activeEffect.duration,
-      type: activeEffect.duration?.type,
-      requiresAbility: activeEffect.duration?.requiresAbility
-    });
   });
 }
 
