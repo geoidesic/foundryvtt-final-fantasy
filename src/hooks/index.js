f9a344494f09a0eab02b9de231565c5828900be9
Noel da Costa <noeldacosta@gmail.com>
2025-03-10 15:46:20 +0000

    moved methods to the automation module

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -5,7 +5,6 @@ import updateCombatant from './updateCombatant.js';
 import canvasReady from './canvasReady.js';
 import ready from './ready.js';
 import init from './init.js';
-import effects from '~/src/helpers/effects';
 
 // Export effects as an object
 export default {
@@ -27,30 +26,3 @@ export {
 };
 
 console.log("[FFXIV] | [HOOKS] Setting up hooks");
-
-/**
- * Hook that runs when damage is applied
- */
-function onDamage() {
-  Hooks.on("FFXIV.onDamage", async (event) => {
-    const actor = event.actor;
-    if (!actor) return;
-
-    const durationManager = new effects.DurationManager(actor);
-    await durationManager.onDamage(event);
-  });
-}
-
-/**
- * Hook that runs when an ability is used
- */
-function onAbilityUse() {
-  console.log("[FFXIV] | [HOOKS] Registering onAbilityUse hook");
-  Hooks.on("FFXIV.onAbilityUse", async (event) => {
-    console.log("[FFXIV] | [ABILITY USE HOOK] Hook triggered", {
-      itemName: event.item?.name,
-      isNewAbilityUse: event.isNewAbilityUse,
-      stack: new Error().stack
-    });
-  });
-}
