e8e956837dc79b7846c65f515a7596ae2af82152
Noel da Costa <noeldacosta@gmail.com>
2025-03-12 09:26:59 +0000

    Release vpatch

--- scripts/release.js
+++ scripts/release.js
@@ -1,5 +1,15 @@
-const fs = require('fs');
-const { execSync } = require('child_process');
+import fs from 'fs';
+import { execSync } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the current directory of the script
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define paths to package.json and system.json
+const packageJsonPath = path.join(__dirname, '../package.json');
+const systemJsonPath = path.join(__dirname, '../system.json');
 
 const version = process.argv[2];
 
@@ -9,13 +19,11 @@ if (!version) {
 }
 
 // Update package.json
-const packageJsonPath = '../package.json';
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 packageJson.version = version;
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
 
 // Update system.json
-const systemJsonPath = '../system.json';
 const systemJson = JSON.parse(fs.readFileSync(systemJsonPath, 'utf8'));
 systemJson.version = version;
 fs.writeFileSync(systemJsonPath, JSON.stringify(systemJson, null, 2));
