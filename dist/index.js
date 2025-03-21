548cd86a992d3a67a980624f86f3531d791f09e7
Noel da Costa <noeldacosta@gmail.com>
2025-03-21 12:30:40 +0000

    Release 0.1.40

--- dist/index.js
+++ dist/index.js
@@ -26238,7 +26238,7 @@ class PopoutSupport {
   }
 }
 PopoutSupport.initialize();
-const version = "0.1.38";
+const version = "0.1.40";
 class WelcomeApplication extends SvelteApplication {
   static {
     __name(this, "WelcomeApplication");
